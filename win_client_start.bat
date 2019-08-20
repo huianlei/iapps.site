669f3f0762965c002f6d75f9ff2d37b196a9ecd4
huianlei <huianlei@gmail.com>
2019-08-21 01:08:45 +0800

    modify default param

--- win_client_start.bat
+++ win_client_start.bat
@@ -7,4 +7,4 @@ rem param "-connect [ip:port]" means: remote server address. required for client
 rem pram "-count 10000" means: total 10000 clients to be started
 rem pram "-concurrent 200" means: start 200 clients  per second
   
-go run app.go -connect localhost:9001 -count 13000 -concurrent 200 
+go run app.go -connect localhost:9001 -count 100 -concurrent 10 
